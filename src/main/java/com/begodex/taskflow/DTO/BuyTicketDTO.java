e74671f0dd049078886b8f0e7fe6d3aaa2a03136
Mordecai456Br <gabrielseixas1@gmail.com>
2025-11-05 20:41:27 -0300

    xao papai
    Remoção das classes antigas

--- src/main/java/com/begodex/taskflow/DTO/BuyTicketDTO.java
+++
@@ -1,7 +0,0 @@
-package com.begodex.taskflow.DTO;
-
-import jakarta.validation.constraints.NotNull;
-
-import java.math.BigDecimal;
-
-public record BuyTicketDTO(@NotNull Long raceId, @NotNull BigDecimal price) {}
