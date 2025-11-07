dda92c50209adf2b34ba37ed7e3dc696cdba2b62
Gabriel Freitas <62771225+Mordecai456Br@users.noreply.github.com>
2025-11-07 16:59:50 -0300

    Guia

--- src/main/java/com/begodex/taskflower/TaskflowApplication.java
+++ src/main/java/com/begodex/taskflower/TaskflowApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+// Mude o application.properties para iniciar o banco de dados. Você deve criar um banco
+// de dados chamado de taskflower e passar suas credênciais.
+
+// http://localhost:8080/swagger-ui/index.html
+// vá em http://localhost:8080/auth/register
+// depois em http://localhost:8080/auth/login para acessar as rotas.
+
 public class TaskflowApplication {
 
 	public static void main(String[] args) {
